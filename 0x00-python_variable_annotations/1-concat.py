2389767c90688252d1b85b6352a0830806db0728
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-07-04 06:22:35 +0100

    concat

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,3 +1,11 @@
-def concat(str1: str, str2: str) -> str:
-    return str1 + str2
+#!/usr/bin/env python3
+"""
+Concatenates two strings and returns the resulting string.
+"""
 
+
+def concat(str1: str, str2: str) -> str:
+    """
+    str1 and str2 are two strings
+    """
+    return str1 + str2
