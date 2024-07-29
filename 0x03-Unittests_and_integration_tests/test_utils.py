7481c59be9101b2ff8d2448ea946cfd372ad2b4c
IbtissamErrachidi <ibtissamerrachidi810@gmail.com>
2024-07-29 23:39:28 +0100

    access_nested_map

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -48,3 +48,29 @@ class TestGetJson(unittest.TestCase):
         result = get_json(test_url)
         self.assertEqual(result, test_payload)
         mock_requests_get.assert_called_once_with(test_url)
+
+class TestMemoize(unittest.TestCase):
+
+    def test_memoize(self):
+        """
+        Test that the memoize decorator correctly caches method results.
+        """
+        class TestClass:
+            
+            def a_method(self):
+                return 42
+            
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        test_instance = TestClass()
+
+        with patch.object(TestClass, 'a_method', return_value=42) as mock_method:
+            result1 = test_instance.a_property()
+            result2 = test_instance.a_property()
+
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+
+            mock_method.assert_called_once()
